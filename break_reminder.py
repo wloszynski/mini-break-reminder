fb659cce215b62004c0f2e05979d39913322954c
wloszynski <adrian@wloszynski.pl>
2020-11-12 12:48:04 +0100

    different messages for 1st  and next breaks

--- break_reminder.py
+++ break_reminder.py
@@ -4,9 +4,29 @@ import time
 import datetime
 from plyer import notification
 
+first_Time = True
 
 if __name__ == '__main__':
     while(True):
+        if first_Time:
+            print('\007')
+            notification.notify(
+                title = 'Hello Dear, have a nice day!',
+                message = 'Break at ' + (datetime.datetime.now() + datetime.timedelta(minutes=30)).strftime('%H:%M:%S'),
+                #displaying time
+                timeout = 2
+            )
+            first_Time = False
+        else:
+            print('\007')
+            notification.notify(
+                title = 'GET BACK TO WORK',
+                message = 'Next break at ' + (datetime.datetime.now() + datetime.timedelta(minutes=30)).strftime('%H:%M:%S'),
+                #displaying time
+                timeout = 2
+            )
+        # sleep for 30 minutes
+        time.sleep(60 * 30)
 
         # \007 -> beep sound
         print('\007')
@@ -17,14 +37,5 @@ if __name__ == '__main__':
             timeout = 2
         )
         # sleep for 5 minutes
-        time.sleep(60 * 5)
-        print('\007')
-        notification.notify(
-            title = 'GET BACK TO WORK',
-            message = str(datetime.datetime.now()) + (datetime.datetime.now() + datetime.timedelta(minutes=30)).strftime('%H:%M:%S'),
-            #displaying time
-            timeout = 2
-        )
+        time.sleep(60 * 5)       
 
-        # sleep for 30 minutes
-        time.sleep(60 * 30)
